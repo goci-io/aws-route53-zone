80c13318fa9a5a674bedba323a1f952faabe1126
etwillbefine <tim.gatzemeier@gmx.de>
2019-09-14 19:09:00 +0200

    do not use remote_state ref if not specified

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ locals {
   prod_stages    = ["prod", "production", "main"]
   use_public     = length(local.vpc_ids) > 0 && var.create_public_zone
   tld            = element(local.domain_parts, length(local.domain_parts) - 1)
-  vpc_ids        = concat(var.zone_vpcs, data.terraform_remote_state.vpc.*.outputs.vpc_id)
   domain_parts   = var.parent_domain_name == "" ? [var.tld] : split(".", var.parent_domain_name)
   fqdn           = var.domain_name == "" ? format("%s.%s", module.label.id, local.tld) : var.domain_name
   public_zone_id = local.use_public ? aws_route53_zone.public_zone[0].zone_id : aws_route53_zone.dns_zone.zone_id
   public_ns      = local.use_public ? aws_route53_zone.public_zone[0].name_servers : aws_route53_zone.dns_zone.name_servers
+  vpc_ids        = var.vpc_module_state == "" ? var.zone_vpcs : concat(var.zone_vpcs, data.terraform_remote_state.vpc.*.outputs.vpc_id)
   label_order    = contains(local.prod_stages, var.stage) && var.omit_prod_stage ? ["name", "attributes", "namespace"] : ["name", "stage", "attributes", "namespace"]
   tag_overwrites = { 
     Name = format("ACM %s", var.name == "" ? local.fqdn : var.name) 
