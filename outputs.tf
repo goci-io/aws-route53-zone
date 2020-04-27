536a14a8d4b64a2091193821ad738696b3e6be4b
Martin Heller <mheller@testo.de>
2020-04-27 13:41:56 +0200

    Remove enabled checks for array-based outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 
 output "zone_id" {
-  value = var.enabled ? join("", aws_route53_zone.dns_zone.*.zone_id) : ""
+  value = join("", aws_route53_zone.dns_zone.*.zone_id)
 }
 
 output "public_zone_id" {
@@ -8,7 +8,7 @@ output "public_zone_id" {
 }
 
 output "name_servers" {
-  value = var.enabled ? aws_route53_zone.dns_zone.*.name_servers : []
+  value = aws_route53_zone.dns_zone.*.name_servers
 }
 
 output "domain_name" {
@@ -16,9 +16,9 @@ output "domain_name" {
 }
 
 output "certificate_arn" {
-  value = var.enabled ? join("", aws_acm_certificate.default.*.arn) : ""
+  value = join("", aws_acm_certificate.default.*.arn)
 }
 
 output "certificate_validation_options" {
-  value = var.enabled ? aws_acm_certificate.default.*.domain_validation_options : []
+  value = aws_acm_certificate.default.*.domain_validation_options
 }
