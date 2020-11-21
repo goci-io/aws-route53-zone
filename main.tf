efc02d2d610b62e24d82db358554f76ebaec6f64
Tim Gatzemeier <etwillbefine@users.noreply.github.com>
2020-11-21 16:40:11 +0100

    update null label module to 0.21.0
    supporting terraform > 0.13

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ data "terraform_remote_state" "vpc" {
 }
 
 module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.21.0"
   enabled     = var.enabled
   namespace   = var.namespace
   stage       = var.stage
